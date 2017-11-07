6ced9f8db61bd53bc386ef0d295c9e8c0881b40d
STUDENTS\1798064 <mj_hadi@yahoo.com>
2017-11-07 12:16:48 -0500

    no message

--- admin/admin.php
+++ admin/admin.php
@@ -1,20 +1,18 @@
 <?php
 
-// Slim creation and setup
-$app = new \Slim\Slim(array(
-    'view' => new \Slim\Views\Twig()
-        ));
-
+// fake $app, $log so that Netbeans can provide suggestions while typing code
+if (false) {
+    $app = new \Slim\Slim();
+    $log = new Logger('main');
+}
 
 if (!isset($_SESSION['user'])) {
     $_SESSION['user'] = array();
 }
 
-
-
 $app->get('/admin/logout', function() use ($app) {
     $_SESSION['user'] = array();
-    $app->render('logout.html.twig', array('userSession' => $_SESSION['user']));
+    $app->render('logout_admin.html.twig', array('userSession' => $_SESSION['user']));
 });
 
 $app->get('/admin/login', function() use ($app) {
@@ -43,23 +41,22 @@ $app->post('/admin/login', function() use ($app) {
 });
 
 $app->get('/isemailregistered/:email', function($email) use ($app) {
-    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $row = DB::queryFirstRow("SELECT * FROM admin WHERE email=%s", $email);
     echo!$row ? "" : '<span style="background-color: red; font-weight: bold;">Email already taken</span>';
 });
-//register user
+
+//Register admin
 $app->get('/admin/register', function() use ($app) {
-    $app->render('register.html.twig');
+    $app->render('/admin/register_admin.html.twig');
 });
 
 $app->post('/admin/register', function() use ($app) {
     $name = $app->request()->post('name');
-    $email = $app->request()->post('email');
-    $address= $app->request()->post('address');
-    $phone= $app->request()->post('phone');
+    $email = $app->request()->post('email');    
     $pass1 = $app->request()->post('pass1');
     $pass2 = $app->request()->post('pass2');
     //
-    $values = array('name' => $name, 'email' => $email, 'address' => $address, 'phone'=> $phone);
+    $values = array('name' => $name, 'email' => $email);
     $errorList = array();
     //
     if (strlen($name) < 2 || strlen($name) > 50) {
@@ -70,7 +67,7 @@ $app->post('/admin/register', function() use ($app) {
         $values['email'] = '';
         array_push($errorList, "Email must look like a valid email");
     } else {
-        $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+        $row = DB::queryFirstRow("SELECT * FROM admin WHERE email=%s", $email);
         if ($row) {
             $values['email'] = '';
             array_push($errorList, "Email already in use");
@@ -78,20 +75,20 @@ $app->post('/admin/register', function() use ($app) {
     }
     if ($pass1 != $pass2) {
         array_push($errorList, "Passwords don't match");
-    } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
+    } else { 
         if (strlen($pass1) < 2 || strlen($pass1) > 50) {
             array_push($errorList, "Password must be between 2 and 50 characters long");
         }
     }
     //
     if ($errorList) { // 3. failed submission
-        $app->render('/Admin/register.html.twig', array(
+        $app->render('/admin/register_admin.html.twig', array(
             'errorList' => $errorList,
             'v' => $values));
     } else { // 2. successful submission
         $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
-        DB::insert('users', array('name' => $name, 'email' => $email, 'password' => $passEnc));
-        $app->render('register_success.html.twig');
+        DB::insert('admin', array('name' => $name, 'email' => $email, 'password' => $passEnc));
+        $app->render('/admin/register_admin_success.html.twig');
     }
 });
 $app->run();
