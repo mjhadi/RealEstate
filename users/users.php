c407432b578a19919cccc17433e1d1b34af3f05a
STUDENTS\1795845 <mhira.ali.hana@gmail.com>
2017-11-07 12:17:32 -0500

    * work in login - register user

---
+++ users/users.php
@@ -0,0 +1,84 @@
+<?php
+
+// fake $app, $log so that Netbeans can provide suggestions while typing code
+if (false) {
+    $app = new \Slim\Slim();
+    $log = new Logger('main');
+}
+$app->get('/login', function() use ($app) {
+    $app->render('login.html.twig');
+});
+
+$app->post('/login', function() use ($app) {
+    $email = $app->request()->post('email');
+    $pass = $app->request()->post('pass');
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $error = false;
+    if (!$row) {
+        $error = true; // user not found
+    } else {
+        if (password_verify($pass, $row['password']) == FALSE) {
+            $error = true; // password invalid
+        }
+    }
+    if ($error) {
+        $app->render('login.html.twig', array('error' => true));
+    } else {
+        unset($row['password']);
+        $_SESSION['user'] = $row;
+        $app->render('login_success.html.twig', array('userSession' => $_SESSION['user']));
+    }
+});
+
+$app->get('/isemailregistered/:email', function($email) use ($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="background-color: red; font-weight: bold;">Email already taken</span>';
+});
+//register user
+$app->get('/Admin/register', function() use ($app) {
+    $app->render('/user/register.html.twig');
+});
+
+$app->post('/User/register', function() use ($app) {
+    $name = $app->request()->post('name');
+    $email = $app->request()->post('email');
+    $address= $app->request()->post('address');
+    $phone= $app->request()->post('phone');
+    $pass1 = $app->request()->post('pass1');
+    $pass2 = $app->request()->post('pass2');
+    //
+    $values = array('name' => $name, 'email' => $email, 'address' => $address, 'phone'=> $phone);
+    $errorList = array();
+    //
+    if (strlen($name) < 2 || strlen($name) > 50) {
+        $values['name'] = '';
+        array_push($errorList, "Name must be between 2 and 50 characters long");
+    }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+        $values['email'] = '';
+        array_push($errorList, "Email must look like a valid email");
+    } else {
+        $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+        if ($row) {
+            $values['email'] = '';
+            array_push($errorList, "Email already in use");
+        }
+    }
+    if ($pass1 != $pass2) {
+        array_push($errorList, "Passwords don't match");
+    } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
+        if (strlen($pass1) < 2 || strlen($pass1) > 50) {
+            array_push($errorList, "Password must be between 2 and 50 characters long");
+        }
+    }
+    //
+    if ($errorList) { // 3. failed submission
+        $app->render('/User/register.html.twig', array(
+            'errorList' => $errorList,
+            'v' => $values));
+    } else { // 2. successful submission
+        $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
+        DB::insert('users', array('name' => $name, 'email' => $email, 'password' => $passEnc));
+        $app->render('register_success.html.twig');
+    }
+});
