14bbc265b4299dd47eafe9cfa0d9631af66ca7b9
STUDENTS\1798064 <mj_hadi@yahoo.com>
2017-11-07 11:50:54 -0500

    ** admin

--- admin/admin.php
+++ admin/admin.php
@@ -1,8 +1,98 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Slim creation and setup
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+        ));
+
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['user'] = array();
+}
+
+
+
+$app->get('/admin/logout', function() use ($app) {
+    $_SESSION['user'] = array();
+    $app->render('logout.html.twig', array('userSession' => $_SESSION['user']));
+});
+
+$app->get('/admin/login', function() use ($app) {
+    $app->render('login_admin.html.twig');
+});
+
+$app->post('/admin/login', function() use ($app) {
+    $email = $app->request()->post('email');
+    $pass = $app->request()->post('pass');
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $error = false;
+    if (!$row) {
+        $error = true; // user not found
+    } else {
+        if (password_verify($pass, $row['password']) == FALSE) {
+            $error = true; // password invalid
+        }
+    }
+    if ($error) {
+        $app->render('login.html.twig', array('error' => true));
+    } else {
+        unset($row['password']);
+        $_SESSION['user'] = $row;
+        $app->render('login_success.html.twig', array('userSession' => $_SESSION['user']));
+    }
+});
+
+$app->get('/isemailregistered/:email', function($email) use ($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="background-color: red; font-weight: bold;">Email already taken</span>';
+});
+//register user
+$app->get('/admin/register', function() use ($app) {
+    $app->render('register.html.twig');
+});
+
+$app->post('/admin/register', function() use ($app) {
+    $name = $app->request()->post('name');
+    $email = $app->request()->post('email');
+    $address= $app->request()->post('address');
+    $phone= $app->request()->post('phone');
+    $pass1 = $app->request()->post('pass1');
+    $pass2 = $app->request()->post('pass2');
+    //
+    $values = array('name' => $name, 'email' => $email, 'address' => $address, 'phone'=> $phone);
+    $errorList = array();
+    //
+    if (strlen($name) < 2 || strlen($name) > 50) {
+        $values['name'] = '';
+        array_push($errorList, "Name must be between 2 and 50 characters long");
+    }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+        $values['email'] = '';
+        array_push($errorList, "Email must look like a valid email");
+    } else {
+        $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+        if ($row) {
+            $values['email'] = '';
+            array_push($errorList, "Email already in use");
+        }
+    }
+    if ($pass1 != $pass2) {
+        array_push($errorList, "Passwords don't match");
+    } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
+        if (strlen($pass1) < 2 || strlen($pass1) > 50) {
+            array_push($errorList, "Password must be between 2 and 50 characters long");
+        }
+    }
+    //
+    if ($errorList) { // 3. failed submission
+        $app->render('/Admin/register.html.twig', array(
+            'errorList' => $errorList,
+            'v' => $values));
+    } else { // 2. successful submission
+        $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
+        DB::insert('users', array('name' => $name, 'email' => $email, 'password' => $passEnc));
+        $app->render('register_success.html.twig');
+    }
+});
+$app->run();
 
