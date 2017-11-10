62642d25ba3055de2362a231fb08f8fceef0c8dd
STUDENTS\1798064 <mj_hadi@yahoo.com>
2017-11-10 13:58:06 -0500

    Add search on the property page

--- property.php
+++ property.php
@@ -160,7 +160,7 @@ $app->get('/property/googlemaps', function() use ($app) {
         $app->render("access_denied.html.twig");
         return;
     }
-         $userId = $_SESSION['user']['userId'];
+   $userId = $_SESSION['user']['userId'];
    $propertyList = DB::query("SELECT * FROM property WHERE userId =%i", $userId);
     if (!$propertyList) {
         $app->render("/not_found.html.twig");
@@ -187,3 +187,30 @@ $app->get('/property/googlemap/:id', function($id) use ($app) {
 });
 
 
+// Search Property
+
+$app->get('/property/search', function() use ($app, $log) {
+    if (!$_SESSION['user']) {
+        $app->render("access_denied.html.twig");
+        return;
+    }
+    $app->render('/property/search_property.html.twig');
+});
+
+$app->post('/property/search', function() use ($app, $log) {
+    if (!$_SESSION['user']) {
+        $app->render("access_denied.html.twig");
+        return;
+    }
+    $search = $app->request()->post('search');
+    $values = array('search' => $search);
+    if (isset($_GET['location'])) {
+        // Checkbox is selected
+        $values = DB::query('SELECT * from property WHERE latitude LIKE %ss AND longitude LIKE %ss', $search, $search);
+    }
+    if (isset($_GET['price'])) {
+        // Checkbox is selected
+        $values = DB::query('SELECT * from property WHERE price LIKE %ss', $search);
+    }    
+    $app->render('/property/search_property.html.twig', array('v' => $values));
+});
